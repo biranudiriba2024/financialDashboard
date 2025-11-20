58e1bbe773a89e9a842b1a508fefef3698756dd3
biranudiriba2024 <biranudiriba2024@gmail.com>
2025-11-20 06:37:18 -0800

    fine

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import Link from "next/link";
 
 import Image from "next/image";
 import { geistSans } from "./fonts";
+import AcmeLogo from "@/components/ui/acme-logo";
+import { connectedToMongodb } from "./(server)/startup/db";
 
-export default function Page() {
+export default async function Page() {
+  const result = await connectedToMongodb();
+  console.log("MongoDB connected:", result.connection.host);
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <h1>ACME</h1>
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
